c0e3aafbf15bcf88974e19eacee4d942ad8c3a35
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2020-07-04 19:55:23 -0400

    Initial Commit

---
+++ RoutinesLifeOrganizer/RoutinesLifeOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinesLifeOrganizerApp.swift
+//  RoutinesLifeOrganizer
+//
+//  Created by Morgan Wilkinson on 7/4/20.
+//
+
+import SwiftUI
+
+@main
+struct RoutinesLifeOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
