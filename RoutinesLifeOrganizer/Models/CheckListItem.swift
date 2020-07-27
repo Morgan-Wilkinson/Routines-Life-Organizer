32a674880c960c8b66ebb4cd4899a37ee1a8d5a2
Morgan-Wilkinson <46545893+Morgan-Wilkinson@users.noreply.github.com>
2020-07-27 15:22:30 -0400

    Updates

---
+++ RoutinesLifeOrganizer/Models/CheckListItem.swift
@@ -0,0 +1,13 @@
+//
+//  CheckListItem.swift
+//  RoutinesLifeOrganizer
+//
+//  Created by Morgan Wilkinson on 7/26/20.
+//
+
+import SwiftUI
+
+struct CheckListItem {
+    var isChecked: Bool = false
+    var title: String
+}
--- RoutinesLifeOrganizer/Views/Supporting View and Modifers/NotesOptions.swift
+++ RoutinesLifeOrganizer/Views/Supporting View and Modifers/NotesOptions.swift
@@ -12,46 +12,69 @@ struct NotesOptions: View {
     // Frequency?
     @Binding var hasFreq: Bool
     @Binding var frequency: Frequency
-    
+    @State private var showFreqPicker: Bool = true
     // Time
     @Binding var hasTime: Bool
     @Binding var timeOfOccurrence: Date
+    @State private var showTimePicker: Bool = true
     
     // Add to Routine Group
     @Binding var selectedGroup: RoutineGroup
     @State private var selectedIndex: Int = 0
+    @State private var showGroupChanger: Bool = false
+    
+    var formatter: DateFormatter {
+        let format = DateFormatter()
+        format.dateStyle = .full
+        format.timeStyle = .short
+        //format.dateFormat = "E, d/MMM/y, HH:mm"
+        return format
+    }
+    
     var body: some View {
         Form {
-            // Time of Occurance?
-            Toggle(isOn: $hasTime) {
-                Text("Remind me on a day")
-            }
-            if hasTime {
-                DatePicker("Alarm", selection: $timeOfOccurrence)
-                    .labelsHidden()
-                    .datePickerStyle(WheelDatePickerStyle())
-                    
-                // Frequency?
-                Toggle(isOn: $hasFreq) {
-                    Text("Remind me at time")
+            Section(header: Text("Set Reminders")) {
+                // Time of Occurance?
+                Toggle(isOn: $hasTime) {
+                    Text("Remind me on a day")
                 }
-                if hasFreq {
-                    Picker("Frequency", selection: $frequency) {
-                        Text("Daily").tag(Frequency.Daily)
-                        Text("Weekly").tag(Frequency.Weekly)
-                        Text("Biweekly").tag(Frequency.Biweekly)
-                        Text("Monthly").tag(Frequency.Monthly)
-                        Text("Every 3 Months").tag(Frequency.Every3Months)
-                        Text("Every 6 Months").tag(Frequency.Every6Months)
-                        Text("Yearly").tag(Frequency.Yearly)
-                    }.labelsHidden()
-                    .pickerStyle(WheelPickerStyle())
+                if hasTime {
+                    DisclosureGroup("\(formatter.string(from: timeOfOccurrence))", isExpanded: $showTimePicker) {
+                        DatePicker("Alarm", selection: $timeOfOccurrence)
+                            .datePickerStyle(WheelDatePickerStyle())
+                            .labelsHidden()
+                    }
                 }
             }
+                Section {
+                    // Frequency?
+                    Toggle(isOn: $hasFreq) {
+                        Text("Remind me at time")
+                    }
+                    if hasFreq {
+                        DisclosureGroup("\(frequency.rawValue)", isExpanded: $showFreqPicker) {
+                            Picker("Frequency", selection: $frequency) {
+                                Text("Daily").tag(Frequency.Daily)
+                                Text("Weekly").tag(Frequency.Weekly)
+                                Text("Biweekly").tag(Frequency.Biweekly)
+                                Text("Monthly").tag(Frequency.Monthly)
+                                Text("Every 3 Months").tag(Frequency.Every3Months)
+                                Text("Every 6 Months").tag(Frequency.Every6Months)
+                                Text("Yearly").tag(Frequency.Yearly)
+                            }.pickerStyle(WheelPickerStyle())
+                            .labelsHidden()
+                        }
+                    }
+                }
             
-            Picker("Add to Routine Group", selection: $selectedIndex) {
-                ForEach(model.routineGroups) { group in
-                    Text(group.name)
+            Section(header: Text("Change Routine Group")) {
+                DisclosureGroup("\(model.routineGroups[selectedIndex].name)", isExpanded: $showGroupChanger) {
+                    Picker(selection: $selectedIndex, label: Text("Routine Group")) {
+                        ForEach(0 ..< model.routineGroups.count) {
+                            Text(model.routineGroups[$0].name)
+                        }
+                    }.pickerStyle(WheelPickerStyle())
+                    .labelsHidden()
                 }
             }
         }
@@ -64,6 +87,9 @@ struct NotesOptions_Previews: PreviewProvider {
         let hasTime: Bool = true
         let timeOfOccurrence = Date()
         let model = Model()
-        NotesOptions(hasFreq: .constant(hasFreq), frequency: .constant(Frequency.Daily), hasTime: .constant(hasTime), timeOfOccurrence: .constant(timeOfOccurrence), selectedGroup: .constant(model.routineGroups[0])).environmentObject(model)
+        
+        NavigationView {
+            NotesOptions(hasFreq: .constant(hasFreq), frequency: .constant(Frequency.Daily), hasTime: .constant(hasTime), timeOfOccurrence: .constant(timeOfOccurrence), selectedGroup: .constant(model.routineGroups[0])).environmentObject(model)
+        }
     }
 }
